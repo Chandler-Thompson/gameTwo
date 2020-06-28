54dec4a1d551f7b322ec3f875e94ecc56645be40
Chandler <chandler.dfys@gmail.com>
2020-06-27 23:59:51 -0400

    Movement fixed, Camera fixed, Shooting Fixed

--- ThemeTwo/Assets/Scripts/Player.cs
+++ ThemeTwo/Assets/Scripts/Player.cs
@@ -2,24 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : PlayerMovement
+public class Player : MonoBehaviour
 {
 
     //basic information of Entity
-    [SerializeField]
-    private float closestDist;
-    [SerializeField]
-    private float furthestDist;
-    [SerializeField]
-	private int health;
-    [SerializeField]
-    private bool isLeft;
-    [SerializeField]
+    [SerializeField] private int health;
+    [SerializeField] private bool isLeft;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float bulletSpeed;
+
     private GameObject followPoint;
-    [SerializeField]
-    private GameObject bullet;
-    [SerializeField]
-    private float bulletSpeed;
+
+    public bool isRight(){
+        return !isLeft;
+    }
 
     public void takeDamage(int amount){
 		health -= amount;
@@ -34,40 +30,14 @@ public class Player : PlayerMovement
     }
 
     // Start is called before the first frame update
-    protected override void Start()
+    void Start()
     {
-        base.Start();
+        followPoint = this.transform.parent.gameObject;
     }
 
     // Update is called once per frame
-    protected override void Update()
+    void Update()
     {
-        base.Update();
-
-        bool moveCloser = Input.GetKey(KeyCode.Q);
-        bool moveFurther = Input.GetKey(KeyCode.E);
-
-        Vector3 closerVector = ((moveCloser && isLeft) ? Vector3.right : (moveCloser && !isLeft) ? Vector3.left : Vector3.zero);
-        Vector3 furtherVector = ((moveFurther && isLeft) ? Vector3.left : (moveFurther && !isLeft) ? Vector3.right : Vector3.zero);
-
-        //check that partner isn't too close or too far away
-        float dist = Vector3.Distance(followPoint.transform.position, this.transform.position);
-
-        if(dist >= furthestDist)
-            furtherVector = Vector3.zero;
-
-        if(dist <= closestDist)
-            closerVector = Vector3.zero;
-
-        //move closer or further away
-        Vector3 direction = furtherVector + closerVector;
-
-        Vector3 playerVelocity = direction * speed * Time.deltaTime;
-
-        float xPosition = GetComponent<Rigidbody2D>().position.x;
-        float yPosition = GetComponent<Rigidbody2D>().position.y;
-
-        GetComponent<Rigidbody2D>().position = new Vector2(xPosition + playerVelocity.x, yPosition + playerVelocity.y);
 
         if(Input.GetMouseButtonDown(0)){
             //Pew Pew angle
@@ -79,7 +49,7 @@ public class Player : PlayerMovement
             //Create the bullet
             GameObject bulletInstance = (GameObject)Instantiate(
                 bullet, 
-                transform.position + (Vector3)(direction*0.5f),
+                transform.position + (Vector3)(shootDirection*0.5f),
                 Quaternion.identity);
 
             //Adds velocity to the bullet
--- ThemeTwo/Assets/Scripts/PlayerMovement.cs
+++ ThemeTwo/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    protected float speed;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float separationSpeed;
+    [SerializeField] private float closestDist;
+    [SerializeField] private float furthestDist;
+
+    [SerializeField] private Player leftPlayer;
+    [SerializeField] private Player rightPlayer;
 
     // Start is called before the first frame update
-    protected virtual void Start()
+    void Start()
     {
         
     }
 
     // Update is called once per frame
-    protected virtual void Update()
+    void FixedUpdate()
     {
+
+        /*
+        
+            Rotate towards mouse
+
+        */
+
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+                                                        mouse.x, 
+                                                        mouse.y,
+                                                        mouse.z));
+        Vector3 forward = -mouseWorld + this.transform.position;
+
+        var newRotation = Quaternion.LookRotation(forward, Vector3.forward);
+        newRotation.y = 0.0f;
+        newRotation.x = 0.0f;
+
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+
+        /*
+        
+            Q E Movement
+
+        */
+
+        bool moveCloser = Input.GetKey(KeyCode.Q);
+        bool moveFurther = Input.GetKey(KeyCode.E);
+
+        //closer and further in terms of the left player
+        Vector3 closerVector = ((moveCloser) ? Vector3.right : Vector3.zero);
+        Vector3 furtherVector = ((moveFurther) ? Vector3.left : Vector3.zero);
+
+        //check that partner isn't too close or too far away, only need to check one
+        float dist = Vector3.Distance(leftPlayer.transform.position, this.transform.position);
+
+        if(dist >= furthestDist)
+            furtherVector = Vector3.zero;
+
+        if(dist <= closestDist)
+            closerVector = Vector3.zero;
+
+        //move closer or further away
+        Vector3 leftDirection = furtherVector + closerVector;
+        Vector3 rightDirection = -(furtherVector + closerVector);//invert vectors for rightPlayer
+
+        Vector3 leftVelocity = leftDirection * separationSpeed * Time.deltaTime;
+        Vector3 rightVelocity = rightDirection * separationSpeed * Time.deltaTime;
+
+        Debug.Log(leftVelocity.ToString("F4") + " | " + rightVelocity.ToString("F4"));
+
+        leftPlayer.transform.Translate(leftVelocity);
+        rightPlayer.transform.Translate(rightVelocity);
+
+        /*
+        
+            WASD Movement
+        
+        */
+
         bool moveUp = Input.GetKey(KeyCode.W);// || Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Mouse Y") > 0;
         bool moveLeft = Input.GetKey(KeyCode.A);// || Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Mouse X") < 0;
         bool moveRight = Input.GetKey(KeyCode.D);// || Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Mouse X") > 0;
@@ -27,9 +92,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 rightVector = ((moveRight) ? Vector3.right:Vector3.zero);
         Vector3 downVector = ((moveDown) ? Vector3.down:Vector3.zero);
 
-        Vector3 direction = upVector + leftVector + rightVector + downVector;
+        Vector3 moveDirection = upVector + leftVector + rightVector + downVector;
 
-        Vector3 playerVelocity = direction * speed * Time.deltaTime;
+        Vector3 playerVelocity = moveDirection * movementSpeed * Time.deltaTime;
 
         float xPosition = GetComponent<Rigidbody2D>().position.x;
         float yPosition = GetComponent<Rigidbody2D>().position.y;
