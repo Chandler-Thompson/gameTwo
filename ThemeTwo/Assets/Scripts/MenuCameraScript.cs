57e438b80b8be145e810e0e6684a0a33c1bd4267
Conningemu <krint@yahoo.com>
2020-06-28 02:18:35 -0400

    Menu In Progress

---
+++ ThemeTwo/Assets/Scripts/MenuCameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraScript : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == transform.position)
+        {
+            target = new Vector3(Random.Range(-17, 5), Random.Range(-5, 4), -10);
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }
+    }
+}
